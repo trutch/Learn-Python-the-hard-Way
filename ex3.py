c893a05991792d359087260e5ddada46e0fbaf2b
Tyler Rutschman <rutschman@alderaan.(none)>
2011-06-14 13:30:14 -0500

    Initial Commit

---
+++ ex3.py
@@ -0,0 +1,39 @@
+# Print a string, stating that chicken counting
+# will commence.
+print "I will now count my chickens:"
+
+# Display the number of Hens  
+print "Hens", 25 + 30 / 6
+# Display the number of Roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+# Print a string stating that egg counting 
+# will commence
+print "Now I will count the eggs:"
+
+# Print the count of the number of eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Print string questioning the truth of a formula
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculate and print the result of previous formula
+print 3 + 2 < 5 - 7
+
+# Question the answer to simple math
+print "What is 3 + 2?", 3 + 2
+# Question the answer to more math
+print "What is 5 - 7?", 5 - 7
+
+# Print string stating it is false
+print "Oh, that's why it's False."
+
+# Print string asking for some more
+print "How about some more."
+
+# Print greater than string and result
+print "Is it greater?", 5 > -2
+# Print greater or equal to string and result
+print "Is it greater or equal?", 5 >= -2
+# Print less than or equal to string and result
+print "Is it less or equal?", 5 <= -2
